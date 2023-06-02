6d6eae0ec753bb23af66a76614027030416e7197
andrzej-semi <andrzej.semi@gmail.com>
2023-06-02 17:15:02 +0100

    Update serverbuild.ps1

--- 101-ServerBuild/serverbuild.ps1
+++ 101-ServerBuild/serverbuild.ps1
@@ -18,11 +18,11 @@ $webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
 & $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
 
 # Set languages/culture. Not needed perse.
-Set-WinSystemLocale en-AU
-Set-WinUserLanguageList -LanguageList en-AU -Force
-Set-Culture -CultureInfo en-AU
-Set-WinHomeLocation -GeoId 12
-Set-TimeZone -Name "E. Australia Standard Time"
+Set-WinSystemLocale en-GB
+Set-WinUserLanguageList -LanguageList en-GB -Force
+Set-Culture -CultureInfo en-GB
+Set-WinHomeLocation -GeoId 242
+Set-TimeZone -Name "GMT Standard Time"
 
 # restart virtual machine to apply regional settings to current user. You could also do a logoff and login.
 Start-sleep -Seconds 40
