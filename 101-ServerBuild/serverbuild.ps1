538b0864989148541ae47e95e8885211390d1c4a
andrzej-semi <andrzej.semi@gmail.com>
2023-10-06 14:22:51 +0100

    Update serverbuild.ps1
    added RMM links

--- 101-ServerBuild/serverbuild.ps1
+++ 101-ServerBuild/serverbuild.ps1
@@ -24,6 +24,20 @@ Set-Culture -CultureInfo en-GB
 Set-WinHomeLocation -GeoId 242
 Set-TimeZone -Name "GMT Standard Time"
 
+# Setup RDS policies
+#Set Time limit for logoff of RemoteApp sessions 
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v RemoteAppLogoffTimeLimit /t REG_DWORD /d 3600000 /f
+#Set time limit for disconnected session
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v MaxDisconnectionTime /t REG_DWORD /d 3600000 /f
+#Set time limit for active but idle Remote Desktop Services sessions
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v MaxIdleTime /t REG_DWORD /d 3600000 /f
+
+#install CW Control
+msiexec /i "https://connect.newlineasp.com/Bin/ScreenConnect.ClientSetup.msi?e=Access&y=Guest&c=Newline&c=Azure&c=&c=&c=United%20Kingdom&c=&c=&c=NAS%20Server" /q
+#install Datto RMM
+(New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/e7f63885-e0f6-40e2-a9e4-afc821d1066b", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
+
+
 # restart virtual machine to apply regional settings to current user. You could also do a logoff and login.
 Start-sleep -Seconds 40
 Restart-Computer
